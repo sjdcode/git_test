a283454e1503d66c0c0c57ffd7927a4bf1c79100
xiafeng <sun2059839873@163.com>
2022-09-30 22:35:41 +0800

    删除全部User

--- src/main/java/com/sjd/comtroller/FirstController.java
+++ src/main/java/com/sjd/comtroller/FirstController.java
@@ -8,4 +8,8 @@ public class FirstController {
         System.out.println("4");
         System.out.println("5");
     }
+
+    public void delectAllUser(){
+        System.out.println("删除全部用户");
+    }
 }
