962f2f8013dd4e4f2df94164a6fdb1657329cecf
xiafeng <sun2059839873@163.com>
2022-09-30 21:42:15 +0800

    四次输出hello Git版本

---
+++ src/main/java/com/sjd/controller/FirstController.java
@@ -0,0 +1,14 @@
+package com.sjd.controller;
+
+public class FirstController {
+    public static void main(String[] args) {
+        System.out.println("hello git !");
+        System.out.println();
+        System.out.println("hello git !");
+        System.out.println();
+        System.out.println("hello git !");
+        System.out.println();
+        System.out.println("hello git !");
+        System.out.println();
+    }
+}
